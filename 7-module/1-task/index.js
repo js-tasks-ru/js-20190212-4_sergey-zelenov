696c70929984bffb262d56a53036f57dc2fc256f
javascript-ru <47786167+jsru@users.noreply.github.com>
2019-03-08 08:18:48 +0100

    Added task makeLogging(fn, log)

---
+++ 7-module/1-task/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Функция декоратор makeLogging(fn, log), для функции fn
+ * возвращающий обёртку, которая при каждом вызове добавляет её аргументы в массив log.
+ * @param {Function} fn - декорируемая функция
+ * @param {Array} log - массив для записи логов
+ * @return {Function}
+ */
+function makeLogging(fn, log) {
+
+}
